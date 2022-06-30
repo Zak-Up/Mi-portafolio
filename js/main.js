bff9d9927faa24f961eb55488d6c9cea2f24d6c7
Zak <andresjr195@hotmail.com>
2022-06-29 19:06:01 -0500

    update

--- js/main.js
+++ js/main.js
@@ -273,6 +273,20 @@ const maquina = (text = '', tiempo = 200, tag = '') => {
 
 maquina('Andres Restrepo', 200, h1_name);
 
+/* =============== LANGUAGE ================= */
+var check = document.querySelector('.check');
+check.addEventListener('click', idioma);
+
+function idioma(){
+  let id = check.checked;
+
+  if(id == true){
+    location.href='/es/index.html'
+  } else {
+    location.href='../index.html'
+  }
+}
+
 /*---------======== SOUND =======------------ */ 
 // function sound(){
 //   let music = new Audio();
